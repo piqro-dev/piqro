138c3f48715e915c24f9407e25256970a110468b
Karcsii1 <137827808+Karcsii1@users.noreply.github.com>
2025-09-23 22:54:30 +0200

    Add files via upload

---
+++ build.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+clang main.cpp -c -o main.o -nostdlib --target=arm-none-eabi -mcpu=cortex-m0+ -mthumb -Oz
+ld.lld --entry 0x20040001 -T boot_stage2.ld main.o -o main.elf
+llvm-objdump -D main.elf > main.list
+llvm-objcopy -O binary main.elf main.bin
+picotool uf2 convert main.bin main.uf2 -o 0x20040000 --family rp2040
+del main.elf main.list main.bin main.o
