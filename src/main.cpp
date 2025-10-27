8b626f16647dbbbe2449a336703909e4ca636d75
Losy001 <64610343+Losy001@users.noreply.github.com>
2025-10-27 05:26:37 +0100

    f

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,31 @@
 #include "picovga.h"
 
-static uint8_t framebuffer[]
+static uint8_t screen_buffer[512 * 400];
+
+void set_pixel(uint16_t x, uint16_t y, uint8_t c)
+{
+	screen_buffer[x + y * 512] = c;
+}
 
 int main()
 {
-	// Initialize video mode
-	Video(DEV_VGA, RES_EGA, FORM_8BIT, Framebuffer);
+	Video(DEV_VGA, RES_EGA, FORM_8BIT, screen_buffer);
+
+	float v = 0.0f;
+
+	while (true)
+	{
+		v += 0.01f;
+
+		WaitVSync();
+
+		__builtin_memset(screen_buffer, 255, sizeof(screen_buffer));
+		
+		for (size_t i = 0; i < 512; i++)
+		{
+			set_pixel(i, 200 + (__builtin_sinf(v + i * 0.1f) * 10.0f), 67);
+		}
+
+
+	}
 }
